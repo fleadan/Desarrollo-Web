5f1be200ac4def2ac44654387358730cb845a8b3
Carlos Daniel Cuellar <34011338+fleadan@users.noreply.github.com>
2020-05-11 19:02:00 -0500

    signin comms

--- trash/src/app/modules/modulos-secundarios/signin/signin.component.ts
+++ trash/src/app/modules/modulos-secundarios/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+  }
+
+  enviar(){
+    this.dataService.nombreUsuario = 'Diego'
   }
 
 }
